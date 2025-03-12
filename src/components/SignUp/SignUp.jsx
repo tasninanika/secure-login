ec1f5138a4511e9bf37f2037145859df01b2756c
anikatasnin <jarintasnin09@gmail.com>
2025-03-12 16:39:12 +0600

    create sign up

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,22 +2,31 @@ import React from "react";
 
 const SignUp = () => {
   return (
-    <div className="flex justify-center my-10">
-      <div className="hero">
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
-            <fieldset className="fieldset">
-              <label className="fieldset-label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="fieldset-label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
-          </div>
-        </div>
+    <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl mt-20">
+      <div className="card-body">
+        <h2 className="text-3xl text-center font-semibold">Sign Up</h2>
+        <form>
+          <fieldset className="fieldset">
+            <label className="fieldset-label">Email</label>
+            <input
+              type="email"
+              className="input"
+              placeholder="Email"
+              required
+            />
+            <label className="fieldset-label">Password</label>
+            <input
+              type="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Signup</button>
+          </fieldset>
+        </form>
       </div>
     </div>
   );
